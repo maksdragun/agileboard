11eae5facea0999134b6b858784fd70918f766e9
maksdragun <MDragunWork@gmail.com>
2018-08-18 23:55:06 +0300

    #02 add main page and controller, add mongo db properties

--- src/main/java/ua/dragun/agileboard/AgileboardApplication.java
+++ src/main/java/ua/dragun/agileboard/AgileboardApplication.java
@@ -1,12 +1,28 @@
 package ua.dragun.agileboard;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class})
+@PropertySource(value = {"classpath:application.properties"})
 public class AgileboardApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.getDefault());
 		SpringApplication.run(AgileboardApplication.class, args);
 	}
 }
